f72b06aa146017f9e4306925d5d451b95f79d46d
Augusto Cesar <guto.slv2@gmail.com>
2024-01-28 11:34:15 -0300

    initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://computer-database.gatling.io/computers',
+    specPattern: 'cypress/e2e/**/*.spec.{js,ts}',
+    watchForFileChanges: false,
+    retries: { runMode: 2, openMode: 2 },
+  },
+});
